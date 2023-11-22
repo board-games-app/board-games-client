dab31651dd1c2bd6d11dd1058d436bdc8b22f886
Júlia Sendra <juliasendra98@gmail.com>
2023-11-22 17:51:45 +0100

    fixes in dice page

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+import diceside1 from "../assets/dice/dice-side-1.png";
+import diceside2 from "../assets/dice/dice-side-2.png";
+import diceside3 from "../assets/dice/dice-side-3.png";
+import diceside4 from "../assets/dice/dice-side-4.png";
+import diceside5 from "../assets/dice/dice-side-5.png";
+import diceside6 from "../assets/dice/dice-side-6.png";
 
 function Dice() {
     const [result, setResult] = useState(null);
@@ -19,13 +25,22 @@ function Dice() {
         }, 500);
     }
 
+    const diceSides = {
+        1: diceside1,
+        2: diceside2,
+        3: diceside3,
+        4: diceside4,
+        5: diceside5,
+        6: diceside6
+    };
+
     return (
         <div className="short-page">
             <div className="dice-container">
                 <div>
                     {result !== null && (
                         <img
-                            src={`./src/assets/dice/dice-side-${result}.png`}
+                            src={diceSides[result]}
                             alt={`Dice side ${result}`}
                             className="dice-image"
                         />
