8fa75ad962960b40fdb09b08787154a224089ef0
Isabel Robleda <isabel.robleda@gmail.com>
2023-11-22 10:03:19 +0100

    addition of daisyui in package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx}", 'node_modules/daisyui/dist/**/*.js',
+  'node_modules/react-daisyui/dist/**/*.js'],
   safelist: ["./src/components/Navbar.jsx"],
  
   daisyui: {
